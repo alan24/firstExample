bbccb3aa295b957e3fdd95886e7c24133bcb1bd7
Alan <alanlim25@gmail.com>
2015-09-29 15:58:41 +0000

    Third test

--- test/controllers/quotes_controller_test.rb
+++ test/controllers/quotes_controller_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class QuotesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   	test "quote show page" do
+     	quote = Quote.create(:author => 'Jon Gruden', :saying => 'It all starts 
+     		with the golden goose.')
+     	get :show, :id => quote.id
+     	assert_response :success
+   	end
+
+   	test "quote show page, not found" do
+   		get :show, :id => 'OMG'
+   		assert_response :not_found
+	end
 end
