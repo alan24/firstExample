76b8e08d28f49c80b2ca5132db29cca5261bd509
Alan <alanlim25@gmail.com>
2015-09-29 15:37:09 +0000

    Second test

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-   test "the truth" do
-     assert true
-   end
+   
+	test "unique_tag" do
+   		quote = Quote.create(:author => 'John Fox', :saying => 'We need to generate more than 
+   			zero points to win games')
+ 		expected = 'JF#' + quote.id.to_s
+ 		actual = quote.unique_tag
+ 		assert_equal expected, actual
+   	end
+
 end
